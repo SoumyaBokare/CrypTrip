20b7c0b020196bc3fca9c13d6eb802c72463ca85
Soumya Bokare <115855925+SoumyaBokare@users.noreply.github.com>
2024-04-14 02:20:19 +0530

    Update Cryptrip.sol

--- Contracts/Cryptrip.sol
+++ Contracts/Cryptrip.sol
@@ -18,52 +18,62 @@ contract RideSharing is Ownable {
         uint totalPayments;
     }
 
-    struct Ride {
+    struct RideRequest {
         uint id;
         address requester;
-        address driver;
+        uint origin; // Location (hashed for privacy)
+        uint destination; // Location (hashed for privacy)
         uint startTime;
-        uint endTime;
-        uint fare;
-        bool completed;
+        CarType carType; // You might want to include CarType from your previous code
+        uint maxPrice; // Maximum price rider is willing to pay (in tokens)
+        RideStatus status; // Define RideStatus enum (e.g., Requested, Offered, Accepted, Completed, Cancelled)
+    }
+
+    struct RideOffer {
+        uint rideRequestId;
+        address driver;
+        uint price;
+        uint eta; // Estimated Time of Arrival
     }
 
     mapping(address => User) public users;
     mapping(address => Driver) public drivers;
-    mapping(uint => Ride) public rides;
+    mapping(uint => RideRequest) public rideRequests; // Mapping for ride requests
+    mapping(uint => RideOffer) public rideOffers; // Mapping for ride offers
 
     ERC20 public token;
 
-    uint public rideCount;
+    // No rideCount variable needed since we're not storing historical rides
 
     constructor(string memory _name, string memory _symbol, address _token) {
         token = ERC20(_token);
     }
 
-    function mint(address _requester, address _driver) public onlyOwner {
-        rideCount++;
-        rides[rideCount] = Ride(rideCount, _requester, _driver, block.timestamp, 0, 0, false);
-        token.transfer(_requester, 1);
+    // Function to initiate a ride (combines functionalities from mint and completeRide)
+    function initiateRide(address _driver, uint _rideRequestId) public {
+        RideRequest storage rideRequest = rideRequests[_rideRequestId];
+        require(rideRequest.status == RideStatus.Requested, "Ride already has offers/completed");
+        require(msg.sender == rideRequest.requester, "Only requester can initiate the ride");
+
+        uint startTime = block.timestamp;
+        uint fare = calculateFare(startTime); // Can calculate fare based on current time
+
+        rideRequest.status = RideStatus.Completed;
+        users[rideRequest.requester].totalPayments += fare;
+        drivers[_driver].totalPayments += fare;
+        users[rideRequest.requester].totalRides++;
+        drivers[_driver].totalRides++;
+
+        // Update reputation based on completed ride (logic not implemented yet)
+        updateReputation(rideRequest.requester, true);
+        updateReputation(_driver, false);
+
+        // Transfer token from requester to driver
+        token.transferFrom(rideRequest.requester, _driver, fare);
     }
 
-    function getRide(uint _rideId) public view returns (Ride memory) {
-        return rides[_rideId];
-    }
-
-    function completeRide(uint _rideId) public {
-        Ride storage ride = rides[_rideId];
-        require(ride.driver == msg.sender, "You are not the driver for this ride");
-        require(ride.startTime > 0, "Ride has not started yet");
-        require(ride.endTime == 0, "Ride has already ended");
-        ride.endTime = block.timestamp;
-        ride.fare = calculateFare(ride.startTime, ride.endTime);
-        ride.completed = true;
-        users[ride.requester].totalPayments += ride.fare;
-        drivers[ride.driver].totalPayments += ride.fare;
-    }
-
-    function calculateFare(uint _startTime, uint _endTime) private pure returns (uint) {
-        // Implement your fare calculation logic here
+    function calculateFare(uint _startTime) private pure returns (uint) {
+        // Implement your fare calculation logic here (consider time-based pricing)
     }
 
     function updateReputation(address _userOrDriver, bool _isUser) private {
